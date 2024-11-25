6eac594e06af857d5fa6608ec0f1dd5c30f2267e
yoongseong <hys_3@yahoo.com>
2024-11-25 09:58:29 +0000

    Commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.logo("gallery/logo.png")
+st.title(":clipboard: NTT Com DD Sales Qualification Tool")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
