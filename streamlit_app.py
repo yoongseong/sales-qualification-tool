cfcfef64c35cdeae5d28bbc4e33df931274fc866
yoongseong <hys_3@yahoo.com>
2024-11-26 07:52:10 +0000

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,8 @@ st.set_page_config(
 
 st.logo("gallery/logo.png")
 st.title(":clipboard: NTT Com DD Sales Qualification Tool")
+st.text("")
+st.text("")
 
 def set_stage(stage):
     st.session_state["stage"] = stage
@@ -26,14 +28,14 @@ with GraphDatabase.driver(URI, auth=AUTH) as driver:
 if st.session_state["stage"] == "1-challenge":
     with placeholder.container():
         challenges = ["North", "East", "South", "West"]
-        st.markdown("##### Are you facing any challenges in the following area of your IT infrastructure?")
+        st.markdown("#### Are you facing any challenges in the following area of your IT infrastructure?")
         selection = st.pills("Choose as many as you like", challenges, selection_mode="multi")
         st.text("")
         st.button("Next", on_click=set_stage, args=["2-requirement"])
 elif st.session_state["stage"] == "2-requirement":
     with placeholder.container():
         requirements = ["North", "East", "South", "West"]
-        st.markdown("##### Do you face?")
+        st.markdown("#### Do you face?")
         st.text("")
         st.button("Get recommendation", type="primary", on_click=set_stage, args=["3-recommendation"])
         st.button("Back", on_click=set_stage, args=["1-challenge"])
